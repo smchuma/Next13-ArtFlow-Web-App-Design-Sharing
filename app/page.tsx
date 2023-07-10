fdef162fbfdeac801b57a981bf6e3179d23431cb
Samora L Mchuma <smchuma@usiu.ac.ke>
2023-07-10 17:55:21 +0300

    trying something new

--- app/page.tsx
+++ app/page.tsx
@@ -32,41 +32,49 @@ const Home = async ({ searchParams: { category, endCursor } }: Props) => {
   const session = await getCurrentUser();
   const data = (await fetchAllProjects(category, endCursor)) as ProjectSearch;
   const projectsToDisplay = data?.projectSearch?.edges || [];
+  console.log("session");
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <section className="">
+        <section className="px-32 py-5 border-b-2 border-gray-100 ">
+          <Categories />
+        </section>
+        <p className="no-result text-center py-40">No projects found</p>
+      </section>
+    );
+  }
+
   const pagination = data?.projectSearch?.pageInfo;
 
   return (
     <section>
-      {!session?.user ? <Banner /> : ""}
+      <Banner />
       <section className="px-32 py-5 border-b-2 border-gray-100 ">
         <Categories />
       </section>
-      {projectsToDisplay.length === 0 ? (
-        <p className="no-result text-center py-40">No projects found</p>
-      ) : (
-        <>
-          <section className="projects-grid paddings">
-            {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
-              <ProjectCard
-                key={node?.id}
-                id={node?.id}
-                image={node?.image}
-                title={node?.title}
-                name={node?.createdBy?.name}
-                avatarUrl={node?.createdBy?.avatarUrl}
-                userId={node?.createdBy?.id}
-              />
-            ))}
-          </section>
-          <div className="paddings">
-            <LoadMore
-              startCursor={pagination.startCursor}
-              endCursor={pagination.endCursor}
-              hasPreviousPage={pagination.hasPreviousPage}
-              hasNextPage={pagination.hasNextPage}
-            />
-          </div>
-        </>
-      )}
+      <section className="projects-grid paddings">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={node?.id}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy?.name}
+            avatarUrl={node?.createdBy?.avatarUrl}
+            userId={node?.createdBy?.id}
+          />
+        ))}
+        C
+      </section>
+      <div className="paddings">
+        <LoadMore
+          startCursor={pagination.startCursor}
+          endCursor={pagination.endCursor}
+          hasPreviousPage={pagination.hasPreviousPage}
+          hasNextPage={pagination.hasNextPage}
+        />
+      </div>
     </section>
   );
 };
